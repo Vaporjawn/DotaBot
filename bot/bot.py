046265dbe3134097fc28739d03779d5f22e07d31
DESKTOP-JOKNH4C\jacob <jacobjones129@yahoo.com>
2023-05-03 19:16:05 -0400

    Find fix for character limit in bot responses

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,6 @@
 import discord
 from bot import responses
+import io
 
 intent = discord.Intents.default()
 intent.message_content = True
@@ -9,7 +10,13 @@ async def send_message(message, user_message, is_private):
 
     try:
         response = responses.handle_responses(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+        with io.StringIO(response) as file:
+
+            msg = file.read(1999).strip()
+            while len(msg) > 0:
+
+                await message.author.send(msg) if is_private else await message.channel.send(msg)
+                msg = file.read(1999).strip()
 
     except Exception as e:
         print(e)
