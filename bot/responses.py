405b44b928c4d82bfe1b9901edeffc601167b73b
DESKTOP-JOKNH4C\jacob <jacobjones129@yahoo.com>
2023-05-02 21:32:42 -0400

    Add functionality for return hero id from name

--- bot/responses.py
+++ bot/responses.py
@@ -1,4 +1,5 @@
 from open_dota_api import query_player_rank
+from open_dota_api.get_hero_id import get_hero_id
 
 
 def handle_responses(message) -> str:
@@ -7,19 +8,20 @@ def handle_responses(message) -> str:
     p_message_list = p_message.split()
     command = p_message_list[0]
 
-    if command[0] != '!':
-        if command == '!help':
-            return 'I currently have no functionality \n' \
-                    'In the future, try these commands!:\n' \
-                    '\t!getrank {Steam ID} - return the rank of the user\n' \
-                    '\t!getwinrate {Steam ID} - return the winrate of the user\n' \
-                    '\t!last5 {Steam ID} - return the outcome of the users last 5 games\n' \
-                    '\t!getstanding - return the current rank of the "Wood League Guild\n'
+    if command == '!help':
+        return 'I currently have no functionality \n' \
+                'In the future, try these commands!:\n' \
+                '\t!getrank {Steam ID} - return the rank of the user\n' \
+                '\t!getwinrate {Steam ID} - return the winrate of the user\n' \
+                '\t!last5 {Steam ID} - return the outcome of the users last 5 games\n' \
+                '\t!getstanding - return the current rank of the "Wood League Guild\n'
 
-        if len(p_message_list) != 1:
-            dota_id = p_message_list[1]
+    if len(p_message_list) != 1:
+        dota_id = p_message_list[1:]
 
-            if command == '!getstanding':
-                return 'This is where I would display the guild standing... IF I COULD!'
-            elif command == '!getrank':
-                return query_player_rank.get_player_rank(dota_id)
+        if command == '!getstanding':
+            return 'This is where I would display the guild standing... IF I COULD!'
+        elif command == '!getrank':
+            return query_player_rank.get_player_rank(dota_id)
+        elif command == '!getheroid':
+            return get_hero_id(dota_id)
